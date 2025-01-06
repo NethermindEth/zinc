8ec95c1840417f75ccc28f75d299049b30408399
Ilia Vlasov <elijah.vlasov@gmail.com>
2025-01-06 14:08:18 +0000

    First draft of the field

--- src/field.rs
+++ src/field.rs
@@ -9,6 +9,12 @@ pub struct RandomField<'config> {
     pub value: BigInt<8>,
 }
 
+impl<'config> RandomField<'config> {
+    pub fn new(config: &'config RandomFieldConfig, value: BigInt<8>) -> Self {
+        RandomField { config, value }
+    }
+}
+
 impl<'a, 'config> Add<&'a RandomField<'config>> for RandomField<'config> {
     type Output = RandomField<'config>;
 
@@ -21,6 +27,16 @@ impl<'a, 'b, 'config> Add<&'a RandomField<'config>> for &'b RandomField<'config>
     type Output = RandomField<'config>;
 
     fn add(self, rhs: &'a RandomField<'config>) -> RandomField<'config> {
+        // Here we assume that the elements of a random field are
+        // created using the same RandomFieldConfig.
+
+        let config_ptr_lhs: *const RandomFieldConfig = self.config;
+        let config_ptr_rhs: *const RandomFieldConfig = rhs.config;
+
+        if config_ptr_lhs != config_ptr_rhs {
+            panic!("cannot add field elements of different fields");
+        }
+
         todo!()
     }
 }

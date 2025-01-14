cc8582f76600e99c0f1083741db71793d460fcf1
Matthew Klein <matthew.klein@nethermind.io>
2025-01-14 15:22:01 +0000

    Add `Zero`, `One`  and `Copy` traits for `RandomField` (#6)
    * Add zero and one trait for Random Field
    
    * Address some comments
    
    * Add partial equality
    
    * Derive Copy
    
    * Get rid of some clones due to introduction of copy
    
    * Add Eq trait implementation
    
    * Address some comments
    
    * change definition of is_one()
    
    * Update the format implementation
    
    * Remove partial eq trait bound

--- src/field_config.rs
+++ src/field_config.rs
@@ -29,13 +29,14 @@ pub fn mac_with_carry(a: u64, b: u64, c: u64, carry: &mut u64) -> u64 {
     *carry = (tmp >> 64) as u64;
     tmp as u64
 }
+
 pub struct FieldConfig<const N: usize> {
     /// The modulus of the field.
     pub modulus: BigInt<N>,
 
     /// Let `M` be the power of 2^64 nearest to `Self::MODULUS_BITS`. Then
     /// `R = M % Self::MODULUS`.
-    r: BigInt<N>,
+    pub r: BigInt<N>,
 
     /// R2 = R^2 % Self::MODULUS
     pub r2: BigInt<N>,

8ba5936b9db38a262b382b034dca63a53b987991
Matthew Klein <matthewklein345@gmail.com>
2025-01-14 09:41:21 +0000

    Get rid of some clones due to introduction of copy

--- src/field.rs
+++ src/field.rs
@@ -71,23 +71,15 @@ impl<'config, const N: usize> Add<RandomField<'config, N>> for RandomField<'conf
     }
 }
 
-impl<'a, 'config, const N: usize> Add<&'a RandomField<'config, N>> for RandomField<'config, N> {
-    type Output = RandomField<'config, N>;
-
-    fn add(self, rhs: &'a RandomField<'config, N>) -> RandomField<'config, N> {
-        &self + rhs
-    }
-}
-
 impl<'a, 'config, const N: usize> Add<&'a RandomField<'config, N>> for &RandomField<'config, N> {
     type Output = RandomField<'config, N>;
 
     fn add(self, rhs: &'a RandomField<'config, N>) -> RandomField<'config, N> {
         if rhs.is_zero() {
-            return self.clone();
+            return *self;
         }
         if self.is_zero() {
-            return rhs.clone();
+            return *rhs;
         }
         // Here we assume that the elements of a random field are
         // created using the same RandomFieldConfig.

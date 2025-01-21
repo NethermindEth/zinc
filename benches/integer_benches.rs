0933565cdbf5e5504f7ba0ca867afbcd836e10e6
Matthew Klein <matthewklein345@gmail.com>
2025-01-21 20:36:00 +0000

    Add bench for mutliplying big integer by u32

---
+++ benches/integer_benches.rs
@@ -0,0 +1,42 @@
+#![allow(non_local_definitions)]
+#![allow(clippy::eq_op)]
+
+use ark_ff::UniformRand;
+use criterion::{
+    black_box, criterion_group, criterion_main, AxisScale, BenchmarkId, Criterion,
+    PlotConfiguration,
+};
+
+use zinc::biginteger::BigInteger256;
+
+fn bench_big_integer(group: &mut criterion::BenchmarkGroup<criterion::measurement::WallTime>) {
+    let mut rng = ark_std::test_rng();
+    let biginteger = BigInteger256::rand(&mut rng);
+    let small_integer = u32::rand(&mut rng);
+
+    group.bench_with_input(
+        BenchmarkId::new("Multiply", "BigInteger256"),
+        &(biginteger, small_integer),
+        |b, (biginteger, small_integer)| {
+            b.iter(move || {
+                for _ in 0..10000 {
+                    let result = black_box(biginteger.clone().muln(*small_integer));
+                    black_box(result);
+                }
+            });
+        },
+    );
+}
+
+pub fn field_benchmarks(c: &mut Criterion) {
+    let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
+
+    let mut group = c.benchmark_group("BigIntegerArithmetic");
+    group.plot_config(plot_config);
+
+    bench_big_integer(&mut group);
+    group.finish();
+}
+
+criterion_group!(benches, field_benchmarks);
+criterion_main!(benches);

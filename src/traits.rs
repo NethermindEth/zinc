95aa4cef54d635cdfce21fc4d14929aafa3e3be7
Ilia Vlasov <elijah.vlasov@gmail.com>
2025-02-11 09:36:22 +0000

    Fix comment

--- src/traits.rs
+++ src/traits.rs
@@ -3,6 +3,6 @@ pub trait FromBytes: Sized {
     /// Constructs an instance from a little-endian byte slice.
     fn from_bytes_le(bytes: &[u8]) -> Option<Self>;
 
-    /// Constructs an instance from a little-endian byte slice.
+    /// Constructs an instance from a byte-endian byte slice.
     fn from_bytes_be(bytes: &[u8]) -> Option<Self>;
 }

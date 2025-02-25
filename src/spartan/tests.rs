5d10a050e0f37dd6a87cc8bb5ab7d55d13a95eca
Matthew Klein <96837318+matthew-a-klein@users.noreply.github.com>
2025-02-25 13:23:46 +0000

    Move spartan tests into separate file

---
+++ src/spartan/tests.rs
@@ -0,0 +1,31 @@
+
+use std::str::FromStr;
+
+use crate::{
+    biginteger::BigInt,
+    brakedown::code::BrakedownSpec1,
+    ccs::test_utils::get_dummy_ccs_from_z_length,
+    field_config::FieldConfig,
+    spartan::{structs::ZincProver, SpartanProver},
+    transcript::KeccakTranscript,
+};
+#[test]
+fn test_spartan_prover() {
+    const N: usize = 3;
+    let config: *const FieldConfig<N> = &FieldConfig::new(
+        BigInt::<3>::from_str("312829638388039969874974628075306023441").unwrap(),
+    );
+    let mut rng = ark_std::test_rng();
+    let n = 1 << 13;
+    let (_, ccs, statement, wit) = get_dummy_ccs_from_z_length::<N>(n, &mut rng, config);
+    let mut transcript = KeccakTranscript::new();
+
+    let prover = ZincProver {
+        config,
+        data: std::marker::PhantomData::<BrakedownSpec1>,
+    };
+
+    let proof = prover.prove(&statement, &wit, &mut transcript, &ccs);
+
+    assert!(proof.is_ok())
+}

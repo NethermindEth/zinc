a26a7e5e155d340b0e59ed84aa79fed338b5044a
Matthew Klein <96837318+matthew-a-klein@users.noreply.github.com>
2025-05-06 13:44:44 +0100

    Add doc for ccs module

--- src/ccs.rs
+++ src/ccs.rs
@@ -1,3 +1,5 @@
+//! Defines behviour for the Customisable Constraint System[https://eprint.iacr.org/2023/552]
+//! In this crate we define CCS over the integers and over a RandomField
 pub mod ccs_f;
 pub mod ccs_z;
 pub(crate) mod error;

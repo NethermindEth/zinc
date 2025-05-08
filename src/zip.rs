93b9f510b04b7f14cd880b97eb6d44871a4aaf83
Matthew Klein <96837318+matthew-a-klein@users.noreply.github.com>
2025-05-08 12:41:16 +0100

    Add docs for zip errors

--- src/zip.rs
+++ src/zip.rs
@@ -8,15 +8,15 @@ mod utils;
 mod tests;
 use thiserror::Error;
 #[derive(Clone, Debug, PartialEq, Error)]
+/// Errors that can occur in the Zip PCS scheme
 pub enum ZipError {
+    /// Parameters do not match
     #[error("Invalid PCS param: {0}")]
     InvalidPcsParam(String),
+    /// The proof of an evaluation was not valid
     #[error("Invalid commitment opening: {0}")]
     InvalidPcsOpen(String),
-    #[error("Bad Snark: {0}")]
-    InvalidSnark(String),
-    #[error("Serialization Error: {0}")]
-    Serialization(String),
+    /// Not able to read or write from the shared transcript
     #[error("Transcript failure: {1}")]
     Transcript(std::io::ErrorKind, String),
 }
